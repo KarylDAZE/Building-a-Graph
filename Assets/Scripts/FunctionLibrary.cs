94c68a2ece7af8a6010c2076eff48f92a47ad581
KarylDAZE <980596745@qq.com>
2024-05-04 01:23:04 +0800

    add: funtions with z-axis

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -6,34 +6,35 @@ public static class FunctionLibrary
     public enum FunctionName { Zero, Wave, MultiWave, Ripple }
     private static Function[] functions = { Zero, Wave, MultiWave, Ripple };
 
-    public delegate float Function(float x, float t);
+    public delegate float Function(float x, float z, float t);
     public static Function GetFunction(FunctionName name)
     {
         return functions[(int)name];
     }
 
-    public static float Zero(float x, float t)
+    public static float Zero(float x, float z, float t)
     {
         return 0;
     }
 
-    public static float Wave(float x, float t)
+    public static float Wave(float x, float z, float t)
     {
-        return Sin(PI * (x + t));
+        return Sin(PI * (x + z + t));
     }
 
-    public static float MultiWave(float x, float t)
+    public static float MultiWave(float x, float z, float t)
     {
         float y = Sin(PI * (x + t));
         //Division requires a bit more work than multiplication
         //And constant expressions are already reduced to a single number by the compiler
         y += Sin(2f * PI * (x + t)) * 0.5f;
-        return y * (2f / 3f);
+        y += Sin(4f * PI * (z + t)) * 0.25f;
+        return y / (1 + 0.5f + 0.25f);
     }
 
-    public static float Ripple(float x, float t)
+    public static float Ripple(float x, float z, float t)
     {
-        float d = Abs(x);
+        float d = Sqrt(x * x + z * z);
         float y = Sin(PI * (4f * d - t)) / (1f + 10f * d);
         return y;
     }
--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -16,15 +16,30 @@ public class Graph : MonoBehaviour
     void Start()
     {
         float step = (xRange * 2 - scale) / (resolution - 1);
-        points = new Transform[resolution];
-        for (int i = 0; i < resolution; i++)
-        {
-            Transform point = points[i] = Instantiate(pointPrefab.transform);
-            position.x = i * step + scale * 0.5f - xRange;
-            point.localPosition = position;
-            point.localScale = Vector3.one * scale;
-            point.SetParent(transform, false);
-        }
+        points = new Transform[resolution * resolution];
+        // for (int i = 0,x=0,z=0; i < resolution; i++,x++)
+        // {
+        //     if(resolution==x){
+        //         x=0;
+        //         z++;
+        //     }
+        //     Transform point = points[i] = Instantiate(pointPrefab.transform);
+        //     position.x = x * step + scale * 0.5f - xRange;
+        //     position.z=z * step + scale * 0.5f - xRange;
+        //     point.localPosition = position;
+        //     point.localScale = Vector3.one * scale;
+        //     point.SetParent(transform, false);
+        // }
+        for (int x = 0; x < resolution; x++)
+            for (int z = 0; z < resolution; z++)
+            {
+                Transform point = points[x * resolution + z] = Instantiate(pointPrefab.transform);
+                position.x = x * step + scale * 0.5f - xRange;
+                position.z = z * step + scale * 0.5f - xRange;
+                point.localPosition = position;
+                point.localScale = Vector3.one * scale;
+                point.SetParent(transform, false);
+            }
     }
 
     // Update is called once per frame
@@ -34,7 +49,7 @@ public class Graph : MonoBehaviour
         for (int i = 0; i < points.Length; i++)
         {
             Vector3 pos = points[i].position;
-            pos.y = f(pos.x, Time.time);
+            pos.y = f(pos.x, pos.z, Time.time);
             points[i].position = pos;
         }
     }
