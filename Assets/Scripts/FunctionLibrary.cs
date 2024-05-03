21427171e224b3eeea42ccc6bd0e1068976b4646
KarylDAZE <980596745@qq.com>
2024-05-04 02:15:52 +0800

    add: draw sphere and torus

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 using static UnityEngine.Mathf;
 public static class FunctionLibrary
 {
-    public enum FunctionName { Zero, Wave, MultiWave, Ripple }
-    private static Function[] functions = { Zero, Wave, MultiWave, Ripple };
+    public enum FunctionName { Zero, Wave, MultiWave, Ripple, Sphere, Torus }
+    private static Function[] functions = { Zero, Wave, MultiWave, Ripple, Sphere, Torus };
 
     public delegate Vector3 Function(float u, float v, float t);
     public static Function GetFunction(FunctionName name)
@@ -49,4 +49,26 @@ public static class FunctionLibrary
         return p;
     }
 
+    public static Vector3 Sphere(float u, float v, float t)
+    {
+        float r = 0.5f + 0.5f * Sin(PI * t);
+        float s = Cos(0.5f * PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = Sin(PI * 0.5f * v);
+        p.z = s * Cos(PI * u);
+        return p * r;
+    }
+
+    public static Vector3 Torus(float u, float v, float t)
+    {
+        float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+        float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+        float s = r1 + r2 * Cos(PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r2 * Sin(PI * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
 }
